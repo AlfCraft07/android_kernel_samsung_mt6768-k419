d790b30afe50f5bee507ea535de281dec395c17f
mertsan2007 <110729187+mertsan2007@users.noreply.github.com>
2026-06-29 00:11:19 +0100

    Revert "usb: gadget: rndis: use list_for_each_entry_safe"
    This reverts commit f6281af9d62e128aa6efad29cf7265062af114f2.

--- drivers/usb/gadget/function/rndis.c
+++ drivers/usb/gadget/function/rndis.c
@@ -1075,9 +1075,10 @@ void rndis_free_response(struct rndis_params *params, u8 *buf)
 	rndis_resp_t *r;
 	struct list_head *act, *tmp;
 
-	spin_lock(&params->resp_lock);
-	list_for_each_entry_safe(r, n, &params->resp_queue, list) {
-		if (r->buf == buf) {
+	list_for_each_safe(act, tmp, &(params->resp_queue))
+	{
+		r = list_entry(act, rndis_resp_t, list);
+		if (r && r->buf == buf) {
 			list_del(&r->list);
 			kfree(r);
 		}
@@ -1093,8 +1094,9 @@ u8 *rndis_get_next_response(struct rndis_params *params, u32 *length)
 
 	if (!length) return NULL;
 
-	spin_lock(&params->resp_lock);
-	list_for_each_entry_safe(r, n, &params->resp_queue, list) {
+	list_for_each_safe(act, tmp, &(params->resp_queue))
+	{
+		r = list_entry(act, rndis_resp_t, list);
 		if (!r->send) {
 			r->send = 1;
 			*length = r->length;
